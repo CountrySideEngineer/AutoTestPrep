e95f7cfdaa50ffaed094b3d68a63a32145c65620
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-12 19:34:17 +0900

    Add FunctionBufferTemplate class. Add FunctionBufferTemplate class to generate codes to define buffer to store the test target function called count and the stub method returns. Add unit test project about FunctionBufferTemplate class. Update unit test solution.

---
+++ dev/test/StubCodeGenerator.FunctionBufferTemplate.utest/FunctionBufferTemplate_utest.cs
@@ -0,0 +1,96 @@
+﻿using CodeGenerator.Stub.Rule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestParser.Target;
+
+namespace StubCodeGenerator.FunctionBufferTemplate.utest
+{
+	[TestClass]
+	public class FunctionBufferTemplate_utest
+	{
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TestTransformText_test_001()
+		{
+			var function = new Function()
+			{
+				DataType = "void",
+				Name = "TestFunction"
+			};
+			var template = new CodeGenerator.Stub.Template.FunctionBufferTemplate()
+			{
+				Target = function,
+				Rule = new NameRule(),
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"LONG TestFunction_called_count;\r\n",
+				output);
+		}
+
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TestTransformText_test_002()
+		{
+			var function = new Function()
+			{
+				DataType = "int",
+				Name = "TestFunction"
+			};
+			var template = new CodeGenerator.Stub.Template.FunctionBufferTemplate()
+			{
+				Target = function,
+				Rule = new NameRule(),
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"LONG TestFunction_called_count;\r\n" +
+				"int TestFunction_return_value[STUB_BUFFER_SIZE_1];\r\n",
+				output);
+		}
+
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TestTransformText_test_003()
+		{
+			var function = new Function()
+			{
+				DataType = "void",
+				PointerNum = 1,
+				Name = "TestFunction"
+			};
+			var template = new CodeGenerator.Stub.Template.FunctionBufferTemplate()
+			{
+				Target = function,
+				Rule = new NameRule(),
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"LONG TestFunction_called_count;\r\n" +
+				"void* TestFunction_return_value[STUB_BUFFER_SIZE_1];\r\n",
+				output);
+		}
+
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TestTransformText_test_004()
+		{
+			var function = new Function()
+			{
+				DataType = "int",
+				PointerNum = 1,
+				Name = "TestFunction"
+			};
+			var template = new CodeGenerator.Stub.Template.FunctionBufferTemplate()
+			{
+				Target = function,
+				Rule = new NameRule(),
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"LONG TestFunction_called_count;\r\n" +
+				"int* TestFunction_return_value[STUB_BUFFER_SIZE_1];\r\n",
+				output);
+		}
+	}
+}
---
+++ dev/test/StubCodeGenerator.FunctionBufferTemplate.utest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StubCodeGenerator.FunctionBufferTemplate.utest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StubCodeGenerator.FunctionBufferTemplate.utest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96a6e4c2-6fd4-450f-87c2-aa3cb7b993b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
