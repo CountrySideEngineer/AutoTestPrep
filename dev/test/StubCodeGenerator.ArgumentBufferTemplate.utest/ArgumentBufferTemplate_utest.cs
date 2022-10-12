d19cc2bd41890b926f1e67d85e0e953c90acacf7
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-12 20:42:39 +0900

    Add template about argument buffer. Add ArgumentBufferTemplate class and its unit test project. Update unit test solution file of visual studio.

---
+++ dev/test/StubCodeGenerator.ArgumentBufferTemplate.utest/ArgumentBufferTemplate_utest.cs
@@ -0,0 +1,38 @@
+﻿using CodeGenerator.Stub.Rule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestParser.Target;
+
+namespace StubCodeGenerator.ArgumentBufferTemplate.utest
+{
+	[TestClass]
+	public class ArgumentBufferTemplate_utest
+	{
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void Transform_test_001()
+		{
+			var function = new Function()
+			{
+				Name = "Function",
+			};
+			var argument = new Parameter()
+			{
+				DataType = "int",
+				Name = "Argument",
+			};
+			var rule = new NameRule();
+			var template = new CodeGenerator.Stub.Template.ArgumentBufferTemplate()
+			{
+				Rule = rule,
+				Target = function,
+				Argument = argument,
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"int Function_Argument[STUB_BUFFER_SIZE_1];\r\n",
+				output);
+
+		}
+	}
+}
