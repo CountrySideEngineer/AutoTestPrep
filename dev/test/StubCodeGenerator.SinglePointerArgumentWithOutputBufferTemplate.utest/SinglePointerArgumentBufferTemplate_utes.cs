5b6a365a5b4caef6f81a9f3207ba7cf884ff563f
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-13 23:15:39 +0900

    Update StubCodeGenerator. Add new template, SinglePointerArgumentWithOutputBufferTemplate class to generate codes for buffer to store value to return via pointer and the length of that. Add unit test of the class. Update unit test solution file.

---
+++ dev/test/StubCodeGenerator.SinglePointerArgumentWithOutputBufferTemplate.utest/SinglePointerArgumentBufferTemplate_utes.cs
@@ -0,0 +1,40 @@
+﻿using CodeGenerator.Stub.Rule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestParser.Target;
+
+namespace StubCodeGenerator.SinglePointerArgumentWithOutputBufferTemplate.utest
+{
+	[TestClass]
+	public class SinglePointerArgumentBufferTemplate_utest
+	{
+		[TestMethod]
+		public void TranformText_test_001()
+		{
+			var function = new Function()
+			{
+				Name = "Function",
+			};
+			var argument = new Parameter()
+			{
+				DataType = "int",
+				Name = "Argument",
+			};
+			var rule = new NameRule();
+			var template = new CodeGenerator.Stub.Template.SinglePointerArgumentWithOutputBufferTemplate()
+			{
+				Rule = rule,
+				Target = function,
+				Argument = argument,
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"int Function_Argument[STUB_BUFFER_SIZE_1];\r\n" +
+				"int Function_Argument_value[STUB_BUFFER_SIZE_1][STUB_BUFFER_SIZE_2];\r\n" +
+				"long Function_Argument_value_size[STUB_BUFFER_SIZE_1];\r\n" +
+				"int Function_Argument_return_value[STUB_BUFFER_SIZE_1][STUB_BUFFER_SIZE_2];\r\n" +
+				"long Function_Argument_return_value_size[STUB_BUFFER_SIZE_1];\r\n",
+				output);
+		}
+	}
+}
