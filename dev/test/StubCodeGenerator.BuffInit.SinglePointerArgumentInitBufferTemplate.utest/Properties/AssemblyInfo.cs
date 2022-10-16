7f2ad0ea791d94fda1eb4568755bbf28106bc21f
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-16 16:51:41 +0900

    Add template class generating codes to initialize argument and return value via pointer.

---
+++ dev/test/StubCodeGenerator.BuffInit.SinglePointerArgumentInitBufferTemplate.utest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StubCodeGenerator.BuffInit.SinglePointerArgumentInitBufferTemplate.utest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StubCodeGenerator.BuffInit.SinglePointerArgumentInitBufferTemplate.utest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f337fd5-9342-4532-b476-d45e4f806a13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
