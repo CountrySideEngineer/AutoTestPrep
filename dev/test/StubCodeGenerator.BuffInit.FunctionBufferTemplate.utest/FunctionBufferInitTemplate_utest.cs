a616a495ad89e6cf18b581a8b364a04e96f7de09
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-16 11:58:02 +0900

    Add template class to initialize buffer to store the number the stub method called.

---
+++ dev/test/StubCodeGenerator.BuffInit.FunctionBufferTemplate.utest/FunctionBufferInitTemplate_utest.cs
@@ -0,0 +1,56 @@
+﻿using CodeGenerator.Stub.Rule;
+using CodeGenerator.Stub.Template.BufferInit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestParser.Target;
+
+namespace StubCodeGenerator.BuffInit.FunctionBufferTemplate.utest
+{
+	[TestClass]
+	public class FunctionBufferInitTemplate_utest
+	{
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TransformText_test_001()
+		{
+			var function = new Function()
+			{
+				DataType = "void",
+				Name = "SampleFunction",
+			};
+			var rule = new NameRule();
+			var template = new FunctionBufferInitTemplate()
+			{
+				Target = function,
+				Rule = rule,
+			};
+			string output = template.TransformText();
+
+			Assert.AreEqual(
+				"	SampleFunction_called_count = 0;\r\n",
+				output);
+		}
+
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TransformText_test_002()
+		{
+			var function = new Function()
+			{
+				DataType = "short",
+				Name = "SampleFunction",
+			};
+			var rule = new NameRule();
+			var template = new FunctionBufferInitTemplate()
+			{
+				Target = function,
+				Rule = rule,
+			};
+			string output = template.TransformText();
+
+			Assert.AreEqual(
+				"	SampleFunction_called_count = 0;\r\n",
+				output);
+		}
+	}
+}
