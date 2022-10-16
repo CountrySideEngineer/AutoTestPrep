258ce95c1c9a5bcff9721c8e5eb31b8b6484602f
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-16 16:08:26 +0900

    Add template class generating codes to initialize argument buffer. Add unit test of the class.

---
+++ dev/test/StubCodeGenerator.BuffInit.ArgumentBufferTemplate.utest/ArgumentBufferTemplate_utest.cs
@@ -0,0 +1,41 @@
+﻿using CodeGenerator.Stub.Rule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestParser.Target;
+using BuffInitTemplate = CodeGenerator.Stub.Template.BufferInit;
+
+namespace StubCodeGenerator.BuffInit.ArgumentBufferTemplate.utest
+{
+	[TestClass]
+	public class ArgumentBufferTemplate_utest
+	{
+		[TestMethod]
+		[TestCategory("UnitTest")]
+		public void TransformText_001()
+		{
+			var function = new Function()
+			{
+				DataType = "short",
+				Name = "SampleFunction",
+			};
+			var argument = new Parameter()
+			{
+				DataType = "long",
+				Name = "SampleArgument",
+			};
+			var rule = new NameRule();
+			var template = new BuffInitTemplate.ArgumentBufferTemplate()
+			{
+				Target = function,
+				Argument = argument,
+				Rule = rule,
+			};
+			string output = template.TransformText();
+			Assert.AreEqual(
+				"	for (int index = 0; index < STUB_BUFFER_SIZE_1; index++) {\r\n" +
+				"		SampleFunction_SampleArgument[index] = 0;\r\n" +
+				"	}\r\n",
+				output);
+		}
+	}
+}
