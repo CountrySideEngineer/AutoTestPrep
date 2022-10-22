e563871a9022499d6b949a1b99b616320a680d37
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-23 07:41:49 +0900

    Add template class for initialize buffer method. Add BufferInitMethodTemplate class, a template class to generate codes to initialize buffer of stub.

---
+++ dev/src/StubCodeGenerator/Template/Stub/Source/Part/Code/BufferInitMethodTemplate_code.cs
@@ -0,0 +1,33 @@
+﻿using CodeGenerator.Stub.Rule;
+using CodeGenerator.Stub.Template.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Stub.Template.Stub.Source.Part
+{
+	public partial class BufferInitMethodTemplate
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public BufferInitMethodTemplate() : base()
+		{
+			var rule = new Rule.NameRule();
+			FunctionBufferTemplateFactory = new FuncBufferInitTemplateFactory(rule);
+			ArgumentBufferTemplateFactory = new ArgBufferInitTemplateFactory(rule);
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="rule">Name rule.</param>
+		public BufferInitMethodTemplate(NameRule rule) : base()
+		{
+			FunctionBufferTemplateFactory = new FuncBufferInitTemplateFactory(rule);
+			ArgumentBufferTemplateFactory = new ArgBufferInitTemplateFactory(rule);
+		}
+	}
+}
