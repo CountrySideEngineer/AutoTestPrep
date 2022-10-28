c16e4fb40769a4a1cef32368dc076ae2f0b6c455
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-28 21:34:37 +0900

    Update not added file.

---
+++ dev/src/StubCodeGenerator/Template/Stub/Source/Part/Code/BufferDeclareExternTemplate_code.cs
@@ -0,0 +1,33 @@
+﻿using CodeGenerator.Stub.Rule;
+using CodeGenerator.Stub.Template.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Stub.Template.Stub.Source.Part
+{
+	public partial class BufferDeclareExternTemplate
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public BufferDeclareExternTemplate() : base()
+		{
+			var rule = new Rule.NameRule();
+			FunctionBufferTemplateFactory = new FuncBufferDecExternTemplateFactory(rule);
+			ArgumentBufferTemplateFactory = new ArgBufferDecExternTemplateFactory(rule);
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="rule">Name rule.</param>
+		public BufferDeclareExternTemplate(NameRule rule) : base()
+		{
+			FunctionBufferTemplateFactory = new FuncBufferDecExternTemplateFactory(rule);
+			ArgumentBufferTemplateFactory = new ArgBufferDecExternTemplateFactory(rule);
+		}
+	}
+}
