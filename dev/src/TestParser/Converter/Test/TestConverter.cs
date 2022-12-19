b6c45122db5e6f9512a387513d196ccb7dceb8c6
CountrySideEngineer <deep-sleeper@nifty.com>
2022-12-19 22:03:38 +0900

    Add TestConverter class. Add TestConverter which inherits, implements IContentConverter interface.

---
+++ dev/src/TestParser/Converter/Test/TestConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableReader.TableData;
+
+namespace TestParser.Converter.Test
+{
+	public class TestConverter : IContentConverter
+	{
+		/// <summary>
+		/// Convert test table data to Test object.
+		/// </summary>
+		/// <param name="src">Content of test table.</param>
+		/// <returns>Test object converted from the Content object.</returns>
+		public object Convert(Content src)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
