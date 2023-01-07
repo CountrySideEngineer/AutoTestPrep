ee08ff0da28c70d5a0889e16151a2ac678b2f3bd
CountrySideEngineer <deep-sleeper@nifty.com>
2023-01-07 12:06:44 +0900

    Update ABufferTemplate class. Update ABufferTemplate class by adding method GetTabFormat() and update hte DataTypeFormat() method to use the added method.

--- dev/src/StubCodeGenerator/Template/Code/ABufferTemplate_code.cs
+++ dev/src/StubCodeGenerator/Template/Code/ABufferTemplate_code.cs
@@ -10,14 +10,43 @@ namespace CodeGenerator.Stub.Template
 {
 	public partial class ABufferTemplate
 	{
+		protected const int _tabCount = 3;
+		protected const int _tabSpace = 4;
 		public NameRule Rule { get; set; }
 		public Function Target { get; set; }
 
 		public string DataTypeFormat(string dataType)
 		{
-			string formatted = string.Format("{0, -20}", dataType);
+			string tabFormat = GetTabFormat(dataType);
+			string formatted = string.Format("{0}{1}", dataType, tabFormat);
 
 			return formatted;
+		}
+
+		protected string GetTabFormat(string input)
+		{
+			int len = input.Length;
+			int tabCount = 0;
+			if (len < (_tabCount * _tabSpace))
+			{
+				int spaceLen = (_tabCount * _tabSpace) - len;
+				tabCount = spaceLen / _tabSpace;
+				int leftSpaceLen = spaceLen % _tabSpace;
+				if (0 != leftSpaceLen)
+				{
+					tabCount++;
+				}
+			}
+			else
+			{
+				tabCount = 1;
+			}
+			string format = string.Empty;
+			for (int index = 0; index < tabCount; index++)
+			{
+				format += "\t";
+			}
+			return format;
 		}
 	}
 }
