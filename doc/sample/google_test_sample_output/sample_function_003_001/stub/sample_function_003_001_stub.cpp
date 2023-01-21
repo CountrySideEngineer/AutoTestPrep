6ac25b643b815b2269063ecbde50685650ca2187
CountrySideEngineer <deep-sleeper@nifty.com>
2023-01-22 05:01:21 +0900

    Change google sample output directory name.

--- doc/sample/google_test_sample_output/sample_function_003_001/stub/sample_function_003_001_stub.cpp
+++ doc/sample/google_test_sample_output/sample_function_003_001/stub/sample_function_003_001_stub.cpp
@@ -14,15 +14,19 @@ DWORD			GetLastError_dwErrCode[STUB_BUFFER_SIZE_1];
  */
 void GetLastError_init()
 {
+	//Initialize the number of function calls.
 	GetLastError_called_count = 0;
 
-	//Initialize the buffer to hold the value the GetLastError stub method will return.
+	//Initialize the buffer to hold the values the GetLastError stub method will return.
 	for (int index = 0; index < STUB_BUFFER_SIZE_1; index++) {
 		GetLastError_return_value[index] = 0;
 	}
+
+	//Initialize the buffer for argument dwErrCode.
 	for (int index = 0; index < STUB_BUFFER_SIZE_1; index++) {
 		GetLastError_dwErrCode[index] = 0;
 	}
+
 }
 
 /*
@@ -30,9 +34,16 @@ void GetLastError_init()
  */
 WINBASEAPI _Check_return_ Post_equals_last_error_ DWORD WINAPI GetLastError(_In_ DWORD dwErrCode)
 {
+	//Get and latch the value from buffer.
 	DWORD latchReturn = GetLastError_return_value[GetLastError_called_count];
+
+	//Set argument value to buffer.
 	GetLastError_dwErrCode[GetLastError_called_count] = dwErrCode;
+
+	//Update function call count variable.
 	GetLastError_called_count++;
+
+	//Return latched value.
 	return latchReturn;
 }
 
