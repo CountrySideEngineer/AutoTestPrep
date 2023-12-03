6a4bb3bc6846473f6989cf5837c426f5e9b3641b
CountrySideEngineer <deep-sleeper@nifty.com>
2023-12-03 18:10:03 +0900

    Add new template classes. Add new template class to generate a file which contains one test function. Add new template class to generate a file which contains a function to initialize test parameters.

---
+++ dev/src/GoogleTestDriverCodeGenerator/Template/Code/GoogleTestSourceEachTestCaseTemplate_code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestParser.Data;
+
+namespace CodeGenerator.TestDriver.Template
+{
+    public partial class GoogleTestSourcePerTestFunctionTemplate
+    {
+        /// <summary>
+        /// TestCase object which contains 
+        /// </summary>
+        public TestCase TestCase { get; set; }
+
+        public GoogleTestSourcePerTestFunctionTemplate() { }
+    }
+}
