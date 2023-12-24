b6750135bb20d4f766f1e1d3d0c16117f9297246
CountrySideEngineer <deep-sleeper@nifty.com>
2023-12-24 09:48:55 +0900

    Update solution of StubDriverPlugin. Add project to develop a plugin to generate test driver codes.

---
+++ dev/src/GTestStubDriverPerFunction/GTestStubDriverPerFunction/GTestStubDriverPerFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StubDriverPlugin.GTestStubDriver;
+using CountrySideEngineer.ProgressWindow.Model;
+using CountrySideEngineer.ProgressWindow.Task;
+using CountrySideEngineer.ProgressWindow.Task.Interface;
+
+namespace GTestStubDriverPerFunction.GTestStubDriverPerFunction
+{
+    public class GTestStubDriverPerFunction : GTestStubDriver
+    {
+        protected override void SetupAction(ref AsyncTask<ProgressInfo> task)
+        {
+			task.TaskAction = ((progress) =>
+			{
+				_progress = progress;
+
+				var pluginExecute = new GTestStubDriverPerFunctionExecute();
+				pluginExecute.NotifyPluginProgressDelegate += NotifyParseProgress;
+				pluginExecute.NotifyPluginFinishDelegate += NotifyPluginFinish;
+				_pluginOutput = pluginExecute.Execute(_pluginInput);
+			});
+		}
+	}
+}
