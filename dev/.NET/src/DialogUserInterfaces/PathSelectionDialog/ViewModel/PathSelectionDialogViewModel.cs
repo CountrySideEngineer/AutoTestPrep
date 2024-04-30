8035fdd54ddfce2a5dd7df50bc1a87b60ec0729d
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-30 13:52:17 +0900

    Add view model classes. Add ViewModelBase and PathSelectionDialogViewModel class.

---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/ViewModel/PathSelectionDialogViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathSelectionDialog.ViewModel
+{
+	public class PathSelectionDialogViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Field of window, dialog title.
+		/// </summary>
+		protected string _title = "PathSelectionDialog";
+
+		/// <summary>
+		/// Window, dialog title.
+		/// </summary>
+		public string Title
+		{
+			get => _title;
+		}
+		
+		/// <summary>
+		/// OK button title field.
+		/// </summary>
+		protected string _okTitle = "OK";
+
+		/// <summary>
+		/// OK button title property.s
+		/// </summary>
+		public string OkTitle
+		{
+			get => _okTitle;
+		}
+
+		/// <summary>
+		/// Cancel button title field.
+		/// </summary>
+		protected string _cancelTitle = "Cancel";
+
+		/// <summary>
+		/// Cancel button title property.
+		/// </summary>
+		public string CancelTitle
+		{
+			get => _cancelTitle;
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public PathSelectionDialogViewModel() : base() { }
+	}
+}
---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/ViewModel/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathSelectionDialog.ViewModel
+{
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		/// <summary>
+		/// Raise PropertyChanged event.
+		/// </summary>
+		/// <param name="propertyName">Changed property name.</param>
+		public virtual void RaisePropertyChange([CallerMemberName]string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
