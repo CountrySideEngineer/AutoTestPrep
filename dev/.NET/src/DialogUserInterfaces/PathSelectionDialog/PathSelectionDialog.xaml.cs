372e648e2d09e57b58116ece3a3e0444b62bd722
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-01 09:13:18 +0900

    Update view. Add property to view to allow user to check if a folder is selected or not.

--- dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/PathSelectionDialog.xaml.cs
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/PathSelectionDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PathSelectionDialog.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,42 @@ namespace PathSelectionDialog
 		public PathSelectionDialog()
 		{
 			InitializeComponent();
+		}
+
+		/// <summary>
+		/// User selected path.
+		/// </summary>
+		public string Path
+		{
+			get
+			{
+				try
+				{
+					return ((PathSelectionDialogViewModel)DataContext).InputPath;
+				}
+				catch (Exception)
+				{
+					return string.Empty;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Whether user selected path or not.
+		/// </summary>
+		public bool IsSelected
+		{
+			get
+			{
+				try
+				{
+					return ((PathSelectionDialogViewModel)DataContext).IsSelected;
+				}
+				catch (Exception)
+				{
+					return false;
+				}
+			}
 		}
 	}
 }
