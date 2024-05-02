7262ad7cf8453508373a70ded8701c437694ad81
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-03 08:17:56 +0900

    Update view model class. -Remove property and field no longer used. -Add a property about sub control as NumericUpViewModel. -Update NumericUpDownControl.xaml by removing data context.

--- dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/ViewModel/NumericSelectionDialogViewModel.cs
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/ViewModel/NumericSelectionDialogViewModel.cs
@@ -8,23 +8,37 @@ namespace PathSelectionDialog.ViewModel
 {
 	public class NumericSelectionDialogViewModel : ViewModelBase
 	{
-		protected Int64 _inputValue = 0;
-		public Int64 InputValue { 
-			get => _inputValue;
-			set
-			{
-				_inputValue = value;
-				RaisePropertyChange();
-			}
-		}
+		/// <summary>
+		/// Sub control view model.
+		/// </summary>
+		public NumericUpDownViewModel UpDownViewModel { get; set; } = new NumericUpDownViewModel();
 
 		/// <summary>
 		/// Default constructor
 		/// </summary>
 		public NumericSelectionDialogViewModel() : base() { }
 
-
-
-
+		/// <summary>
+		/// User input value.
+		/// </summary>
+		public Int64 InputValue
+		{
+			get
+			{
+				try
+				{
+					Int64 inputValue = Convert.ToInt64(UpDownViewModel.InputValue);
+					return inputValue;
+				}
+				catch (Exception)
+				{
+					throw;
+				}
+			}
+			set
+			{
+				UpDownViewModel.InputValue = value.ToString();
+			}
+		}
 	}
 }
