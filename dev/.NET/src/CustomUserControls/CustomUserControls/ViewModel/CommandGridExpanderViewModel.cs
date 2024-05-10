1c98b709027483a98f37ae45fcbbd22117628ee8
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-10 17:25:56 +0900

    Update view model class.

--- dev/.NET/src/CustomUserControls/CustomUserControls/ViewModel/CommandGridExpanderViewModel.cs
+++ dev/.NET/src/CustomUserControls/CustomUserControls/ViewModel/CommandGridExpanderViewModel.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace CustomUserControls.ViewModel
 {
-    public class CommandGridExpanderViewModel
+    public class CommandGridExpanderViewModel : ViewModelBase
     {
+		/// <summary>
+		/// Category name field.
+		/// </summary>
+		protected string _categoryName = string.Empty;
+
+		/// <summary>
+		/// Category name property.
+		/// </summary>
+		public string CategoryName
+		{
+			get => _categoryName;
+			set
+			{
+				_categoryName = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public CommandGridExpanderViewModel() : base() { }
     }
 }
