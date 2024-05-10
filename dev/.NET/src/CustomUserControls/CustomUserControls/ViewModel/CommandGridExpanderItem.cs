b38f062722e680442f4b44d57af3b5bde107deac
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-10 14:15:37 +0900

    Add classes. DelegateCommand, CommandGridExpanderItem, and CommandGridExpanderViewModel classes added.

---
+++ dev/.NET/src/CustomUserControls/CustomUserControls/ViewModel/CommandGridExpanderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CustomUserControls.ViewModel
+{
+    public class CommandGridExpanderItem
+    {
+		public string Title { get; set; } = string.Empty;
+
+		public string Item { get; set; } = string.Empty;
+    }
+}
