310c6d96793d01a848159de80e5c32767b71bddf
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-11 10:06:07 +0900

    Add view model classes. Add MainWindowViewModel and ViewModelBase classes are added to the

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.ViewModel
+{
+	internal class MainWindowViewModel : ViewModelBase
+	{
+		public string Title
+		{
+			get => Properties.Resources.IDS_APP_TITLE;
+		}
+
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public MainWindowViewModel() : base() { }
+	}
+}
