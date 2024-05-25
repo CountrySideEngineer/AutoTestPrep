c609fbff97364925af3026ae087a54e492002a5e
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-25 23:04:53 +0900

    Update MainWindowViewModel class. -Add command to be called when the note in tree view is called. -Update constructor to set sample data of tree view item.

--- dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/MainWindowViewModel.cs
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/MainWindowViewModel.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using AutoTestPrep.Command;
+using CustomUserControls.Command;
+using CustomUserControls.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AutoTestPrep.ViewModel
 {
@@ -37,12 +44,128 @@ namespace AutoTestPrep.ViewModel
 			}
 		}
 
+		protected ProjectItemViewModel? _selectedItem = null;
+		public ProjectItemViewModel? SelectedItem
+		{
+			get => _selectedItem;
+			set
+			{
+				_selectedItem = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		protected DelegateCommand<ProjectItemViewModel>? _selectedItemChangedCommand = null;
+		public DelegateCommand<ProjectItemViewModel> SelectedItemChangedCommand
+		{
+			get
+			{
+				if (null == _selectedItemChangedCommand)
+				{
+					_selectedItemChangedCommand = new DelegateCommand<ProjectItemViewModel>(SelectedItemChangedExecute);
+				}
+				return _selectedItemChangedCommand;
+			}
+		}
+
 		/// <summary>
 		/// Default constructor.
 		/// </summary>
 		public MainWindowViewModel() : base()
 		{
-			ProjectRootItem = null;
+			//ProjectRootItem = null;
+			ProjectRootItem = new List<ProjectItemViewModel>()
+			{
+				new ProjectItemViewModel()
+				{
+					Name = "Root",
+					SubProjects = new List<ProjectItemViewModel>()
+					{
+						new ProjectItemViewModel()
+						{
+							Name = "SubItem_001",
+							SubProjects = new List<ProjectItemViewModel>()
+							{
+								new ProjectItemViewModel()
+								{
+									Name = "SubItem_001_001",
+								},
+								new ProjectItemViewModel()
+								{
+									Name = "SubItem_001_002",
+								},
+								new ProjectItemViewModel()
+								{
+									Name = "SubItem_001_003",
+								},
+
+							}
+						}
+					}
+				}
+			};
+
+			var _itemCommandDidctonary = new Dictionary<string, CommandGridExpanderViewModel>()
+			{
+				{
+					"SubItem_001_001", new CommandGridExpanderViewModel()
+					{
+						CategoryName = "SubItem_001_001",
+						Items = new List<CommandGridExpanderItem>()
+						{
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_001_Content_001"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_001_Content_002"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_001_Content_003"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_001_Content_004"
+							},
+						}
+					}
+				},
+				{
+					"SubItem_001_002", new CommandGridExpanderViewModel()
+					{
+						CategoryName = "SubItem_001_002",
+						Items = new List<CommandGridExpanderItem>()
+						{
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_002_Content_001"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_002_Content_002"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_002_Content_003"
+							},
+							new CommandGridExpanderItem()
+							{
+								Title = "SubItem_001_002_Content_004"
+							},
+						}
+					}
+				},
+			};
+		}
+
+		public void SelectedItemChangedExecute(ProjectItemViewModel selectedItem)
+		{
+			Debug.WriteLine($"{nameof(SelectedItemChangedExecute)} is called.");
+			Console.WriteLine($"{nameof(SelectedItemChangedExecute)} is called.");
+
+			SelectedItem = selectedItem;
 		}
 	}
 }
