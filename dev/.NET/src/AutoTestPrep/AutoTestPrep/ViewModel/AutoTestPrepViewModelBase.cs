d0ad674ea96010ef8fbfc150527acad632935bea
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-27 21:54:42 +0900

    Add new view model classes. New view model classes are copied from .NET Framework environment and updated for .NET Core.

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/AutoTestPrepViewModelBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.ViewModel
+{
+	internal class AutoTestPrepViewModelBase : ViewModelBase
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public AutoTestPrepViewModelBase() : base()
+		{
+			ViewModelIndex = 0;
+			IsSelected = true;
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="viewModelIndex">View model index.</param>
+		public AutoTestPrepViewModelBase(int viewModelIndex)
+		{
+			this.ViewModelIndex = viewModelIndex;
+			this.IsSelected = true;
+		}
+
+		/// <summary>
+		/// View model index.
+		/// </summary>
+		public int ViewModelIndex { get; protected set; }
+
+		/// <summary>
+		/// Flag about the view model object has been selected or not.
+		/// </summary>
+		protected bool _IsSelected;
+
+		/// <summary>
+		/// Flag about the view model object has been selected or not.
+		/// </summary>
+		public bool IsSelected
+		{
+			get
+			{
+				return this._IsSelected;
+			}
+			set
+			{
+				this._IsSelected = value;
+				this.RaisePropertyChanged(nameof(IsSelected));
+			}
+		}
+	}
+}
---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/TestInformationInputViewModel.cs
@@ -0,0 +1,58 @@
+﻿using AutoTestPrep.Properties;
+using System.ComponentModel;
+
+namespace AutoTestPrep.ViewModel
+{
+	/// <summary>
+	/// View model about test information input view.
+	/// </summary>
+	internal class TestInformationInputViewModel : AutoTestPrepViewModelBase
+	{
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public TestInformationInputViewModel() : this(-1) { }
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="index"></param>
+		public TestInformationInputViewModel(int index) : base(index) { }
+
+		/// <summary>
+		/// Path to directory which contains source code files to analysis.
+		/// </summary>
+		protected string _inputRootDirPath = string.Empty;
+
+		/// <summary>
+		/// Path to directory which contains source code files to analysis.
+		/// </summary>
+		public string InputRootDirPath
+		{
+			get => _inputRootDirPath;
+			set
+			{
+				_inputRootDirPath = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		/// <summary>
+		/// Path to directory to output test codes.
+		/// </summary>
+		protected string _outputDirPath = string.Empty;
+
+		/// <summary>
+		/// Path to directory to output test codes.
+		/// </summary>
+		public string OutputDirPath
+		{
+			get => _outputDirPath;
+			set
+			{
+				_outputDirPath = value;
+				RaisePropertyChanged();
+			}
+		}
+	}
+}
