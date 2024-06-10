0eac9a726ecb1de4329a999abc9f5b64da72677c
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-10 22:49:18 +0900

    Add LibraryInformationViewModel class.

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/LibraryInformationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CustomUserControls.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.ViewModel
+{
+	internal class LibraryInformationViewModel : AutoTestPrepViewModelBase
+    {
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public LibraryInformationViewModel() : base()
+		{
+			Items = new List<CommandGridExpanderItem>()
+			{
+				new CommandGridExpanderItem()
+				{
+					Title = Properties.Resources.IDS_LIBRARY_FILES
+				},
+				new CommandGridExpanderItem()
+				{
+					Title = Properties.Resources.IDS_LIBRARY_DIRS
+				},
+			};
+		}
+
+		public string LibraryFile
+		{
+			get
+			{
+				return ExtractItem(0);
+			}
+		}
+
+		public string LibraryDirectory
+		{
+			get
+			{
+				return ExtractItem(1);
+			}
+		}
+    }
+}
