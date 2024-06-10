edab1edb6dfda58866c334e6f82c33d0952051f1
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-10 21:07:13 +0900

    Update MainWindow. Replace TestProjectConfigurationView by UserControl. Add and implement control selector class, UserControlSelector which inherits DataTemplateSelector, to show the TestProjectConfigurationView in UserControl in MainWindow. Attention, the implemented codes are sample ones.

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/Controls/UserControlSelector.cs
@@ -0,0 +1,27 @@
+﻿using AutoTestPrep.View;
+using CustomUserControls.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AutoTestPrep.Controls
+{
+	internal class UserControlSelector : DataTemplateSelector
+	{
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			var viewModel = (CommandGridExpanderViewModel)item;
+			var element = (FrameworkElement)container;
+
+			DataTemplate template = (DataTemplate)element.FindResource("ItemSelect_001");
+			UserControl userControl = (UserControl)template.LoadContent();
+			userControl.DataContext = viewModel;
+
+			return template;
+		}
+	}
+}
