0f44b19f8fbedffad4a673f1b5dd8a47e3e863cd
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-13 19:03:53 +0900

    Add view model class. TestProjectConfigInputViewModel class, view model for TestProjectConfigInputView.

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/TestProjectConfigInputViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CustomUserControls.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.ViewModel
+{
+	internal class TestProjectConfigInputViewModel : AutoTestPrepViewModelBase
+	{
+		public CommandGridExpanderViewModel TestInformation { get; set; }
+
+		public CommandGridExpanderViewModel TestDoubleInformation { get; set; }
+
+		public CommandGridExpanderViewModel HeaderinDriverInformation { get; set; }
+
+		public CommandGridExpanderViewModel HeaderInTestDoubleInformation { get; set; }
+
+		public CommandGridExpanderViewModel LibraryInformation { get; set; }
+
+		public CommandGridExpanderViewModel MacroInformation { get; set; }
+
+		public TestProjectConfigInputViewModel()
+		{
+			TestInformation = new TestInformationInputViewModel();
+			TestDoubleInformation = new BufferSizeViewModel();
+			HeaderinDriverInformation = new TestDriverHeaderInformationViewModel();
+			HeaderInTestDoubleInformation = new TestStubHeaderInformationViewModel();
+			LibraryInformation = new LibraryInformationViewModel();
+			MacroInformation = new MacroInformationViewModel();
+		}
+	}
+}
