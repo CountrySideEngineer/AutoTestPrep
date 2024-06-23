aed11e4763cf245808d8d59b643388c1ef954bc0
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-23 21:55:37 +0900

    Add a new command class to select folder.

---
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/Command/PathSelectionCommand.cs
@@ -0,0 +1,30 @@
+﻿using CustomUserControls.Command;
+using DialogUserInterfaces.View;
+using DialogUserInterfaces.ViewModel;
+
+namespace AutoTestPrep.Command
+{
+	internal class PathSelectionCommand : ICustomUserCommand<string>
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public PathSelectionCommand() { }
+
+		/// <summary>
+		/// Select folder path.
+		/// </summary>
+		/// <param name="parameter">Command parameter.</param>
+		/// <returns>Path to folder selected by user.</returns>
+		public string Execute(string parameter)
+		{
+			var pathSelectView = new PathSelectionDialog();
+			pathSelectView.ShowDialog();
+
+			var context = (PathSelectionDialogViewModel)pathSelectView.DataContext;
+			var selectedPath = context.InputPath;
+
+			return selectedPath;
+		}
+	}
+}
