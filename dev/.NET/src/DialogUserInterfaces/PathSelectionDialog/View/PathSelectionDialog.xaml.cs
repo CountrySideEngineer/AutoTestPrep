803db4ae72f56370b47b413f7c4fc9336e5fa89a
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-23 21:47:03 +0900

    Change view class pass.

--- dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/PathSelectionDialog.xaml.cs
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/PathSelectionDialog.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace DialogUserInterfaces
+namespace DialogUserInterfaces.View
 {
 	/// <summary>
 	/// PathSelectionDialog.xaml の相互作用ロジック
