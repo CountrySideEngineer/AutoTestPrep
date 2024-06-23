02149a2ea8ed3d7004b5f9ebfede550cc6e677ba
CountrySideEngineer <deep-sleeper@nifty.com>
2024-06-24 06:51:17 +0900

    Update BufferSizeViewModel. Set command to show a dialog to set numeric value. Replace default buffer size by a field "_defaultBufferSize1" and "_defaultBufferSize2".

--- dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/BufferSizeViewModel.cs
+++ dev/.NET/src/AutoTestPrep/AutoTestPrep/ViewModel/BufferSizeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CustomUserControls.ViewModel;
+﻿using AutoTestPrep.Command;
+using CustomUserControls.ViewModel;
+using DialogUserInterfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,16 @@ namespace AutoTestPrep.ViewModel
 {
 	internal class BufferSizeViewModel : AutoTestPrepViewModelBase<long>
 	{
+		/// <summary>
+		/// Default buffer1 size.
+		/// </summary>
+		protected long _defaultBufferSize1 = 100;
+
+		/// <summary>
+		/// Default buffer2 size.
+		/// </summary>
+		protected long _defaultBufferSize2 = 100;
+
 		/// <summary>
 		/// Default constructor.
 		/// </summary>
@@ -21,12 +33,14 @@ namespace AutoTestPrep.ViewModel
 				new CommandGridExpanderItem<long>()
 				{
 					Title = Properties.Resources.IDS_TEST_DOUBLE_BUFFER_SIZE_1,
-					Item = 100
+					Item = _defaultBufferSize1,
+					CustomCommand = new NumericSelectionCommand()
 				},
 				new CommandGridExpanderItem<long>()
 				{
 					Title = Properties.Resources.IDS_TEST_DOUBLE_BUFFER_SIZE_2,
-					Item = 100
+					Item = _defaultBufferSize2,
+					CustomCommand = new NumericSelectionCommand()
 				}
 			};
 		}
