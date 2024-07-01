308faa651363e76f41ee38d513dc1677c4a2cd72
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-01 22:39:02 +0900

    Add command to show a dialog to select path.

---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/Command/PathSelectionCommand.cs
@@ -0,0 +1,48 @@
+﻿using DialogUserInterfaces.View;
+using DialogUserInterfaces.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogUserInterfaces.Command
+{
+    public class PathSelectionCommand : IDialogCommand<string>
+    {
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public PathSelectionCommand() { }
+
+		/// <summary>
+		/// Show PathSelection dialog and return the path selected in the dialog.
+		/// </summary>
+		/// <param name="parameter">Default command value.</param>
+		/// <returns>Selected path in the dialog.</returns>
+		public string Execute(string parameter)
+		{
+			var dialog = new PathSelectionDialog();
+			bool? result = dialog.ShowDialog();
+
+			if (null == result)
+			{
+				return parameter;
+			}
+			else
+			{
+				if (result.Value)
+				{
+					var viewModel = (PathSelectionDialogViewModel)dialog.DataContext;
+					var selectedPath = viewModel.InputPath;
+
+					return selectedPath;
+				}
+				else
+				{
+					return parameter;
+				}
+			}
+		}
+	}
+}
