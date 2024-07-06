5e2cb9f1eadfc58c3bf00ccce0062f20b538cc24
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-07 07:32:43 +0900

    Add a property to MultiPathSelectionDialog. Add InputPath property named "InputPath" to get path user input.

--- dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/MultiPathSelectionDialog.xaml.cs
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/MultiPathSelectionDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DialogUserInterfaces.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,18 @@ namespace DialogUserInterfaces.View
 			DialogResult = false;
 
 			Close();
+		}
+
+		/// <summary>
+		/// Content of the dialog.
+		/// </summary>
+		public IEnumerable<string> InputPath
+		{
+			get
+			{
+				IEnumerable<string> content = ((MultiPathSelectionViewModel)DataContext).GetContent();
+				return content;
+			}
 		}
 	}
 }
