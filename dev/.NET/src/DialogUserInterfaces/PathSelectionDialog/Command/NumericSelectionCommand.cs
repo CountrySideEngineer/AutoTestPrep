8de342a26b25ba70b741b9484bf2d42ffec55818
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-07 07:59:18 +0900

    Add new command class. Command about NumericSelectionDialog is added.

---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/Command/NumericSelectionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DialogUserInterfaces.Command
+{
+	public class NumericSelectionCommand : DialogCommand<long>
+	{
+		public NumericSelectionCommand() : base()
+		{
+			_dialog = new NumericSelectionDialog();
+		}
+
+		protected override long GetDialogResult(Window? window)
+		{
+			long result = ((NumericSelectionDialog)_dialog).InputValue;
+
+			return result;
+		}
+
+		public override long Execute(long parameter)
+		{
+			((NumericSelectionDialog)_dialog).InputValue = parameter;
+
+			return base.Execute(parameter);
+		}
+	}
+}
