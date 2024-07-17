1d17096a2c14f85a03e9efa029066c955ea9a908
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-17 21:43:52 +0900

    Start developing view model class of MultiPathInputDialog view.

---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/ViewModel/MultiPathInputViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogUserInterfaces.ViewModel
+{
+    internal class MultiPathInputViewModel : ViewModelBase
+    {
+		protected readonly string _splitter = ";";
+
+		public string Title { get => Properties.Resources.IDS_WINDOW_TITLE; }
+
+		public string OkTitle { get => Properties.Resources.IDS_OK_BUTTON_TITLE; }
+
+		public string CancelTitle { get => Properties.Resources.IDS_CANCEL_BUTTON_TITLE;  }
+
+		protected string _inputPath = string.Empty;	
+
+		public string InputPath
+		{
+			get => _inputPath;
+			set
+			{
+				_inputPath = value;
+				RaisePropertyChange();
+			}
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public MultiPathInputViewModel() : base() { }
+    }
+}
