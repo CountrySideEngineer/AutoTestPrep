0bba382b0b2805cb204c2cbc87ea43cc393b78b7
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-19 07:43:27 +0900

    Update MultiPathInputDialog class. Add and set event handler to button click action. Add data binding to text content in the TextBox control of Text property.

--- dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/MultiPathInputDialog.xaml.cs
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/View/MultiPathInputDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DialogUserInterfaces.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +24,60 @@ namespace DialogUserInterfaces.View
         {
             InitializeComponent();
         }
+
+		/// <summary>
+		/// OK button click event handler.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event argument.</param>
+		private void OKButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+
+			Close();
+		}
+
+		/// <summary>
+		/// Cancel button click event handler.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event argument.</param>
+		private void CancelButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+
+			Close();
+		}
+
+		/// <summary>
+		/// Content of the dialog.
+		/// </summary>
+		public string Path
+		{
+			get
+			{
+				if (null != DialogResult)
+				{
+					if (DialogResult == true)
+					{
+						string path = ((MultiPathInputViewModel)DataContext).GetContent();
+						return path;
+					}
+					else
+					{
+						return string.Empty;
+					}
+				}
+				else
+				{
+					return string.Empty;
+				}
+			}
+			set
+			{
+				var viewModel = (MultiPathInputViewModel)DataContext;
+				viewModel.SetContent(value);
+			}
+		}
     }
 }
