5f6d97ffd21ddabb53d63e5848baf0b80036aa7f
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-19 07:50:23 +0900

    Add command class to use MultiPathInputDialog.

---
+++ dev/.NET/src/DialogUserInterfaces/PathSelectionDialog/Command/MultiPathInputCommand.cs
@@ -0,0 +1,40 @@
+﻿using DialogUserInterfaces.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DialogUserInterfaces.Command
+{
+	public class MultiPathInputCommand : DialogCommand<string>
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public MultiPathInputCommand() : base() { }
+
+		protected override Window GetDialog(string parameter)
+		{
+			var dialog = new MultiPathInputDialog();
+			dialog.Path = parameter;
+
+			return dialog;
+		}
+
+		protected override string GetDialogResult(Window window)
+		{
+			try
+			{
+				var dialog = (MultiPathInputDialog)window;
+				string path = dialog.Path;
+				return path;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
