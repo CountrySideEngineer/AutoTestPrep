55c2bb8d5d9c9b9e83c0ecfa9ea4a0267ae79e06
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-05 21:53:42 +0900

    Add TestCase class.

---
+++ dev/.NET/src/TestParser/TestParser.SDK/Model/Test/TestCase.cs
@@ -0,0 +1,69 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParser.Model.Test
+{
+	public class TestCase
+	{
+		/// <summary>
+		/// Test case ID.
+		/// </summary>
+		public string Id { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Test case input datas.
+		/// </summary>
+		public IEnumerable<TestData> Inputs { get; set; } = new List<TestData>();
+
+		/// <summary>
+		/// Test case expect datas, not output.
+		/// </summary>
+		public IEnumerable<TestData> Expects { get; set; } = new List<TestData>();
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestCase() { }
+
+		/// <summary>
+		/// Copy to other TestCase object.
+		/// </summary>
+		/// <param name="dst">TestCase object to copy to.</param>
+		public virtual void CopyTo(TestCase dst)
+		{
+			Log.TRACE();
+
+			dst.Id = new(Id);
+			dst.Inputs = new List<TestData>(Inputs);
+			dst.Expects = new List<TestData>(Expects);
+		}
+
+		/// <summary>
+		/// Shallow copy method.
+		/// </summary>
+		/// <returns>Shallow copied method.</returns>
+		public virtual TestCase ShallowCopy()
+		{
+			Log.TRACE();
+
+			return (TestCase)MemberwiseClone();
+		}
+
+		/// <summary>
+		/// Deep copy method.
+		/// </summary>
+		/// <returns>Deep copied TestCase object.</returns>
+		public virtual TestCase DeepCopy()
+		{
+			Log.TRACE();
+
+			TestCase testCase = (TestCase)MemberwiseClone();
+			CopyTo(testCase);
+			return testCase;
+		}
+	}
+}
