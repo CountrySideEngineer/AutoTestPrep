362a09d091afaa41ca5123dfda991cc84b46b217
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-14 07:05:32 +0900

    Add IReader interface. Add an interface to read object.

---
+++ dev/.NET/src/TestParser/TestParser.SDK/Reader/IReader.cs
@@ -0,0 +1,36 @@
+﻿namespace TestParser.Reader
+{
+	public interface IReader
+	{
+		/// <summary>
+		/// Read object from path.
+		/// </summary>
+		/// <param name="path">Path to read.</param>
+		/// <returns>Read object.</returns>
+		object Read(string path);
+
+		/// <summary>
+		/// Read object from stream.
+		/// </summary>
+		/// <param name="stream">Path to stream.</param>
+		/// <returns>Read object.</returns>
+		object Read(Stream stream);
+	}
+
+	public interface IReader<T>
+	{
+		/// <summary>
+		/// Read object from path.
+		/// </summary>
+		/// <param name="path">Path to read.</param>
+		/// <returns>Read object.</returns>
+		T Read(string path);
+
+		/// <summary>
+		/// Read object from stream.
+		/// </summary>
+		/// <param name="stream">Path to stream.</param>
+		/// <returns>Read object.</returns>
+		T Read(Stream stream);
+	}
+}
