566c5bbee68c09723db5ef7dc03fb2acafda164b
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-14 07:18:08 +0900

    Change folder, project, and solution structure.

---
+++ dev/.NET/src/TestReader/TestReader/Parser/IParser.cs
@@ -0,0 +1,36 @@
+﻿namespace TestParser.Parser
+{
+	internal interface IParser
+	{
+		/// <summary>
+		/// Parse file specified by argument path.
+		/// </summary>
+		/// <param name="path">Path to file to parse.</param>
+		/// <returns>Parsed object.</returns>
+		object Parse(string path);
+
+		/// <summary>
+		/// Parse file.
+		/// </summary>
+		/// <param name="stream">Stream to parse.</param>
+		/// <returns>Parsed object.</returns>
+		object Parse(Stream stream);
+	}
+
+	internal interface IParser<T>
+	{
+		/// <summary>
+		/// Parse file specified by argument path.
+		/// </summary>
+		/// <param name="path">Path to file to parse.</param>
+		/// <returns>Parsed object.</returns>
+		T Parse(string path);
+
+		/// <summary>
+		/// Parse file.
+		/// </summary>
+		/// <param name="stream">Stream to parse.</param>
+		/// <returns>Parsed object.</returns>
+		T Parse(Stream stream);
+	}
+}
