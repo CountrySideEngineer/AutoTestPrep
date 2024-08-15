2da07feaeed205af98758e5b626d3540d4141b23
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-16 08:05:21 +0900

    Update interface IReader. Add interfaces methods to the IReader interface.

--- dev/.NET/src/TestReader/TestReader.SDK/Reader/IReader.cs
+++ dev/.NET/src/TestReader/TestReader.SDK/Reader/IReader.cs
@@ -15,6 +15,22 @@
 		/// <param name="stream">Path to stream.</param>
 		/// <returns>Read object.</returns>
 		object Read(Stream stream);
+
+		/// <summary>
+		/// Read object from path and specified by name.
+		/// </summary>
+		/// <param name="path">Path to file to read.</param>
+		/// <param name="name">Name to read.</param>
+		/// <returns>Read object.</returns>
+		object Read(string path, string name);
+
+		/// <summary>
+		/// Read object from stream and specified by name.
+		/// </summary>
+		/// <param name="stream">Stream to read from.</param>
+		/// <param name="name">Name to read.</param>
+		/// <returns>Read object.</returns>
+		object Read(Stream stream, string name);
 	}
 
 	public interface IReader<T>
@@ -32,5 +48,21 @@
 		/// <param name="stream">Path to stream.</param>
 		/// <returns>Read object.</returns>
 		T Read(Stream stream);
+
+		/// <summary>
+		/// Read object from file specified by paht and name.
+		/// </summary>
+		/// <param name="path">Path to read.</param>
+		/// <param name="name">Name to read.</param>
+		/// <returns>Read object.</returns>
+		T Read(string path, string name);
+
+		/// <summary>
+		/// Read object from stream and specified by name..
+		/// </summary>
+		/// <param name="stream">Path to stream.</param>
+		/// <param name="name">Name to read.</param>
+		/// <returns>Read object.</returns>
+		T Read(Stream stream, string name);
 	}
 }
