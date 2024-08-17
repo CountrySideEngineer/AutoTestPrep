751ffea44901e12dcb37537d19916f4b08ac7101
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-17 20:39:18 +0900

    Fix name space of Resources in TestReader.SDK.

--- dev/.NET/src/TestReader/TestReader.SDK/Properties/Resources.Designer.cs
+++ dev/.NET/src/TestReader/TestReader.SDK/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TestParser.Properties {
+namespace TestReader.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TestParser.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestParser.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestReader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
