b196fe33f82604440ab288a9edcafd9b828e932e
CountrySideEngineer <deep-sleeper@nifty.com>
2024-09-02 22:11:22 +0900

    Add code generator class.

---
+++ dev/.NET/src/CodeGenerator/GTestDriverCodeGenerator/GoogleTest/CodeGenerator.cs
@@ -0,0 +1,43 @@
+﻿using CodeGenerator.GoogleTest.Template;
+using CodeGenerator.SDK.Data;
+using Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.GoogleTest
+{
+	public class CodeGenerator : ACodeGenerator
+	{
+		public string TestHeaderFileName { get; set; } = string.Empty;
+
+		public string StubHeaderFileName { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public CodeGenerator() : base() { }
+
+		/// <summary>
+		/// Create test driver code template.
+		/// </summary>
+		/// <param name="codeInput">Test code input.</param>
+		/// <returns>Test driver code template.</returns>
+		protected override GoogleTestCodeTemplate GetTemplate(CodeInput codeInput)
+		{
+			Log.TRACE();
+
+			var template = new GoogleTestSourceTestDriverTemplate()
+			{
+				TargetFunction = codeInput.TestDesignData.Target,
+				Test = codeInput.TestDesignData,
+				Config = codeInput.Configuration,
+				DriverHeaderFileName = TestHeaderFileName,
+				StubHeaderFileName = StubHeaderFileName
+			};
+			return template;
+		}
+	}
+}
