aae9b89d82bab6c13fa99427bc3f9e46b27d5ecd
CountrySideEngineer <deep-sleeper@nifty.com>
2024-09-07 15:59:34 +0900

    Start developing test double code generator as library.

---
+++ dev/.NET/src/CodeGenerator/TestDoubleCodeGenerator/Rule/NameRule.cs
@@ -0,0 +1,160 @@
+﻿using Logger;
+using TestReader.Model;
+using Function = TestReader.Model.Function;
+
+namespace TestDoubleCodeGenerator.Rule
+{
+	internal class NameRule
+	{
+		/// <summary>
+		/// Get the name fo variable that keeps track of the number of times the functin has been called.
+		/// </summary>
+		/// <param name="function">Target function.</param>
+		/// <returns>The name of variable.</returns>
+		public static string CalledCount(Function function)
+		{
+			Log.TRACE();
+
+			string calledCount = $"{function.Name}_called_count";
+
+			Log.DEBUG($"{nameof(calledCount),16} = {calledCount}");
+
+			return calledCount;
+		}
+
+		/// <summary>
+		/// Get the name of variable that holds the values returned by the function.
+		/// </summary>
+		/// <param name="function">Target function.</param>
+		/// <returns>The variable name.</returns>
+		public static string ReturnValue(Function function)
+		{
+			Log.TRACE();
+
+			string returnValue = $"{function.Name}_return_value";
+
+			Log.DEBUG($"{nameof(returnValue),16} = {returnValue}");
+
+			return returnValue;
+		}
+
+		/// <summary>
+		/// Get the name of variable that holds the argument value.
+		/// </summary>
+		/// <param name="function">Target function.</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string ArgumentBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that hold the value of the memory region pointed to by a pointer.
+		/// </summary>
+		/// <param name="function">Target function</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string SinglePointerArgumentValueBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_value";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that holds the value 
+		/// </summary>
+		/// <param name="function">Target function.</param>
+		/// <param name="argument">Argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string SinglePointerArgumentValueSizeBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_value_size";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that hold the value the test double will return via pointer argument.
+		/// </summary>
+		/// <param name="function">Target function</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string SingglePointerArgumentReturnValueBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_return_value";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that hold the value the test double will return via pointer argument.
+		/// </summary>
+		/// <param name="function">Target function</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string SingglePointerArgumentReturnValueSizeBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_return_value_size";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that hold the value the test double will return via pointer argument.
+		/// </summary>
+		/// <param name="function">Target function</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string DoublePointerArgumentReturnValueBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_return_value";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+
+		/// <summary>
+		/// Get the name of variable that hold the value the test double will return via pointer argument.
+		/// </summary>
+		/// <param name="function">Target function</param>
+		/// <param name="argument">The argument.</param>
+		/// <returns>The variable name.</returns>
+		public static string DobulePointerArgumentReturnValueSizeBuffer(Function function, Parameter argument)
+		{
+			Log.TRACE();
+
+			string argumentBuffer = $"{function.Name}_{argument.Name}_return_value_size";
+
+			Log.DEBUG($"{nameof(argumentBuffer),16} = {argumentBuffer}");
+
+			return argumentBuffer;
+		}
+	}
+}
