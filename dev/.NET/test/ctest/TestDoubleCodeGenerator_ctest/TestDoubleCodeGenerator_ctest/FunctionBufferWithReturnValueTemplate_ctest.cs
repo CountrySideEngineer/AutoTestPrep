64f7e3ec063e0dd204f94024491cf4491b181329
CountrySideEngineer <deep-sleeper@nifty.com>
2024-09-11 22:35:59 +0900

    Add solution and project to test test double template classes.

---
+++ dev/.NET/test/ctest/TestDoubleCodeGenerator_ctest/TestDoubleCodeGenerator_ctest/FunctionBufferWithReturnValueTemplate_ctest.cs
@@ -0,0 +1,58 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Metrics;
+using System.Text;
+using System.Threading.Tasks;
+using TestDoubleCodeGenerator.TestDouble.Template.Buffer;
+using TestReader.Model;
+
+namespace TestDoubleCodeGenerator_ctest
+{
+	internal class FunctionBufferWithReturnValueTemplate_ctest
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			Log.AddLogger(new Logger.Console.DebugLog());
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			var function = new Function()
+			{
+				Name = "SampleFunction",
+				DataType = "int",
+				PointerNum = 0
+			};
+			var argument = new Parameter()
+			{
+				Name = "Argument1",
+				DataType = "ArgDataType",
+				Mode = Parameter.ACCESS_MODE.OUT,
+				PointerNum = 2
+			};
+
+			var template = new FunctionBufferWithReturnValueTemplate()
+			{
+				Target = function
+			};
+			string code = template.TransformText();
+
+			string expect = $"{"long",-16}\tSampleFunction_called_count;" + Environment.NewLine +
+				$"{"int",-16}\tSampleFunction_return_value[BUFFER_SIZE_1];" + Environment.NewLine;
+
+			Log.DEBUG($"{nameof(code),16}= {Environment.NewLine}{code}");
+			Log.DEBUG($"{nameof(expect),16}= {Environment.NewLine}{expect}");
+
+			Assert.That(code, Is.EqualTo(expect));
+		}
+	}
+}
---
+++ dev/.NET/test/ctest/TestDoubleCodeGenerator_ctest/TestDoubleCodeGenerator_ctest/PointerArgumentBufferTemplate_ctest.cs
@@ -0,0 +1,54 @@
+﻿using Logger;
+using System.Diagnostics;
+using TestDoubleCodeGenerator.TestDouble.Template.Buffer;
+using TestReader.Model;
+
+namespace TestDoubleCodeGenerator_ctest
+{
+	public class PointerArgumentBufferTemplate_ctest
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			Log.AddLogger(new Logger.Console.DebugLog());
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			var function = new Function()
+			{
+				Name = "SampleFunction",
+				DataType = "int",
+				PointerNum = 0
+			};
+			var argument = new Parameter()
+			{
+				Name = "Argument1",
+				DataType = "ArgDataType",
+				PointerNum = 1,
+				Mode = Parameter.ACCESS_MODE.IN
+			};
+
+			var template = new PointerArgumentBufferTemplate()
+			{
+				Target = function,
+				Argument = argument
+			};
+			string code = template.TransformText();
+
+			string expect = $"{"ArgDataType*",-16}\tSampleFunction_Argument1[BUFFER_SIZE_1];" + Environment.NewLine;
+			expect += $"{"ArgDataType",-16}\tSampleFunction_Argument1_value[BUFFER_SIZE_1][BUFFER_SIZE_2];" + Environment.NewLine;
+			expect += $"{"long",-16}\tSampleFunction_Argument1_value_size[BUFFER_SIZE_1];" + Environment.NewLine;
+
+			Log.DEBUG($"{nameof(code),16} = {Environment.NewLine}{code}");
+			Log.DEBUG($"{nameof(expect),16} = {Environment.NewLine}{expect}");
+			Assert.That(code, Is.EqualTo(expect));
+		}
+	}
+}
