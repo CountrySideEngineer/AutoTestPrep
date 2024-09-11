8d5e9f955638c148bec334769e0e16aa554d4968
CountrySideEngineer <deep-sleeper@nifty.com>
2024-09-11 22:11:30 +0900

    Add a new code template class. ATTENTION!! The codes are not implemented.

---
+++ dev/.NET/src/CodeGenerator/TestDoubleCodeGenerator/TestDouble/Template/Init/ArgumentBufferTemplate_Code.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestReader.Model;
+
+namespace TestDoubleCodeGenerator.TestDouble.Template.Init
+{
+	public partial class ArgumentBufferTemplate
+	{
+		/// <summary>
+		/// Argument information
+		/// </summary>
+		public Parameter Argument { get; set; } = new Parameter();
+	}
+}
